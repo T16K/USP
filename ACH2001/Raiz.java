d6f3bb4da17936742ddbb067a7d8393c1c6c579a
T16K <82551584+T16K@users.noreply.github.com>
2021-10-13 08:30:01 -0300

    Update Raiz.java

--- ACH2001/Raiz.java
+++ ACH2001/Raiz.java
@@ -1,22 +1,26 @@
 /*********************************************************************/
-/**   ACH2001 - IntroduÁ„o ‡ ProgramaÁ„o                            **/
+/**   ACH2001 - Introdu√ß√£o √† Programa√ß√£o                            **/
 /**   EACH-USP - Primeiro Semestre de 2020                          **/
 /**   <turma> - <nome do professor>                                 **/
 /**     02      Luciano Digiampietri                                **/
-/**   Segundo ExercÌcio-Programa                                    **/
+/**   Segundo Exerc√≠cio-Programa                                    **/
 /**                                                                 **/
-/**   <nome do(a) aluno(a)>                   <n˙mero USP>          **/
+/**   <nome do(a) aluno(a)>                   <n√∫mero USP>          **/
 /**     Gustavo Tsuyoshi Ariga                  11857215            **/
 /**   <data de entrega>  06/05/20                                   **/
 /*********************************************************************/
 
 /*
-	C·lculo para raiz quadrada
+	C√°lculo para raiz quadrada
 */
 public class Raiz {
 
 	/*
-		Para fazer o programa, a primeira dificuladade seria implementar o conceito do 'i' dado no enunciado, por isso foi utilizado o recurso 'while', uma vez que 'i = 0 => x = a / 2', e os prÛximos valores para 'i' implicam em chamar a mesma vari·vel 'x' atribuindo um novo valor. Depois disso, a condiÁ„o para a repetiÁ„o do 'while' foi determinada a partir de manipulaÁ„o algÈbrica (algumas vezes o programa demorava muito para calcular ent„o a constante da fÛrmula foi removida) para chegar apenas nas vari·veis 'a' e 'x'. E por fim, foi utilizado a fÛrmula para a aproximaÁ„o do valor da raiz quadrada de 'a' dentro do 'while'.
+		Para fazer o programa, a primeira dificuladade seria implementar o conceito do 'i' dado no enunciado, por isso foi utilizado o recurso 'while', 
+		uma vez que 'i = 0 => x = a / 2', e os pr√≥ximos valores para 'i' implicam em chamar a mesma vari√°vel 'x' atribuindo um novo valor. Depois disso, 
+		a condi√ß√£o para a repeti√ß√£o do 'while' foi determinada a partir de manipula√ß√£o alg√©brica (algumas vezes o programa demorava muito para calcular 
+		ent√£o a constante da f√≥rmula foi removida) para chegar apenas nas vari√°veis 'a' e 'x'. E por fim, foi utilizado a f√≥rmula para a aproxima√ß√£o do 
+		valor da raiz quadrada de 'a' dentro do 'while'.
 	*/
 
 	static double raizQuadrada(double a, double epsilon) {
@@ -24,13 +28,13 @@ public class Raiz {
 		// Retorna o valor -1, caso a < 0, epsilon <= 0 ou epsilon >= 1.
 		if (a < 0 || epsilon <= 0 || epsilon >= 1) return -1;
 		
-		// Declarar a vari·vel.
+		// Declarar a vari√°vel.
 		double x = a / 2;
 
-		// Como n„o era possÌvel usar o mÈtodo Math.abs(), foi utilizada a definiÁ„o de mÛdulo.            
+		// Como n√£o era poss√≠vel usar o m√©todo Math.abs(), foi utilizada a defini√ß√£o de m√≥dulo.            
 		while ( a / x - x  > epsilon || a / x - x  < - epsilon )   
 
-		// C·lculo para definir o novo valor de 'x'.
+		// C√°lculo para definir o novo valor de 'x'.
 		x = (a/x + x) / 2;         
 
 		// Retornar o valor de 'x'.
